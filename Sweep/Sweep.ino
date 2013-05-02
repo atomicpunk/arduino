0c3ee6dc8b6650fb0f0729688875bdffe2a4e237
Todd Brandt <todd.e.brandt@intel.com>
2013-05-02 16:12:53 -0700

    added docs and copyrights

--- Sweep/Sweep.ino
+++ Sweep/Sweep.ino
@@ -1,3 +1,25 @@
+/*
+ * Copyright 2012 Todd Brandt <tebrandt@frontier.com>
+ *
+ * This program is free software; you may redistribute it and/or modify it
+ * under the same terms as Perl itself.
+ *    trancearoundtheworld mp3 archive sync utility
+ *    Copyright (C) 2012 Todd Brandt <tebrandt@frontier.com>
+ *
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License along
+ *    with this program; if not, write to the Free Software Foundation, Inc.,
+ *    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
 
 #include <Servo.h> 
 
