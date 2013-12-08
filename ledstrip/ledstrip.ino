da324d02ccbe2e07cd991b12f4493ea64b1f8bce
Todd Brandt <todd.e.brandt@intel.com>
2013-12-08 15:24:31 -0800

    removed sweep an d minimized the camera interface impact

--- ledstrip/ledstrip.ino
+++ ledstrip/ledstrip.ino
@@ -64,21 +64,10 @@
 #define T_SOFTMID 10
 #define T_SOFTMIN 3
 
-// sweep settings
-#define S_DELAY 5
-#define S_PAUSE 20000
-#define S_CMDCHECK 1000
-#define S_INC 1
-#define S_DEC (-1*S_INC)
-
 Servo myservo;
 String cmdString = "";
 int pos=A_RESET, newpos=A_RESET;
 int delayval = 1;
-bool sweep = false;
-int sweepval = S_INC;
-long sweepstop[3] = {
-  10000, 0, 10000};
 
 PROGMEM const unsigned long pattern_red[10][10]={
   { 0xff0000,0x000000,0x000000,0x000000,0x000000,0x000000,0x000000,0x000000,0x000000,0x000000  },
@@ -366,13 +355,7 @@ void runCommand()
 
     if((temp >= A_HARDMIN)&&(temp <= A_HARDMAX))
     {
-      sweep = false;
       newpos = temp;
-      if(!myservo.attached())
-      {
-        Serial.println("Attach");
-        myservo.attach(PIN);
-      }
     }
     else
     {
@@ -380,76 +363,9 @@ void runCommand()
       Serial.println("deg out of range");
     }
     break;
-  case 't':
-    if(data == "slow")
-      temp = T_SOFTMAX;
-    else if(data == "fast")
-      temp = T_SOFTMIN;
-    else if(data == "norm")
-      temp = T_SOFTMID;
-    else if(isNumber(data))
-      temp = data.toInt();
-    else
-    {
-      Serial.print("Malformed command: ");
-      Serial.println(cmdString);
-      break;
-    }
-
-    if((temp >= T_HARDMIN)&&(temp <= T_HARDMAX))
-    {
-      delayval = temp;
-      Serial.print("New delayval: ");
-      Serial.println(delayval);
-    }
-    else
-    {
-      Serial.print(temp);        
-      Serial.println("ms out of range");
-    }
-    break;
   case 'i':
     Serial.print("Angle: ");      
     Serial.println(pos);
-    Serial.print("Delay: ");      
-    Serial.println(delayval);
-    break;
-  case 's':
-    if(cmdString == "s")
-    {
-      sweep = !sweep;
-      if(sweep)
-      {
-        Serial.println("sweep started");
-        myservo.attach(PIN);
-        delayval = S_DELAY;
-        if(pos >= A_SOFTMAX)
-        {
-          newpos = A_SOFTMAX-1;
-          sweepval = S_DEC;
-        }
-        else if(pos <= A_SOFTMIN)
-        {
-          newpos = A_SOFTMIN+1;
-          sweepval = S_INC;
-        }
-        else if(pos == A_SOFTMID)
-        {
-          newpos += sweepval;
-        }
-      }
-      else
-      {
-        Serial.println("sweep stopped");     
-        myservo.detach();
-        delayval = T_SOFTMID;
-      }
-    }
-    else
-    {
-      Serial.print("Malformed command: ");
-      Serial.println(cmdString);
-    }
     break;
   default:
     Serial.print("Unknown command: ");
@@ -475,48 +391,6 @@ void checkForCommand()
   }  
 }
 
-void runSweep()
-{
-  if((pos >= A_SOFTMAX)||(pos <= A_SOFTMIN)||(pos == A_SOFTMID))
-  {
-    long sweepdelay;
-    if(pos >= A_SOFTMAX)
-    {
-      sweepval *= -1;
-      sweepdelay = sweepstop[2]; // right view time
-    }
-    else if(pos <= A_SOFTMIN)
-    {
-      sweepval *= -1;
-      sweepdelay = sweepstop[0]; // left view time
-    }
-    else
-    {
-      sweepdelay = sweepstop[1]; // center view time
-    }
-    if(sweepdelay > 0)
-    {
-      myservo.detach();
-      for(int i = 0; i < sweepdelay; i += S_CMDCHECK)
-      {
-        checkForCommand();
-        if(sweep)
-        {
-          delay(S_CMDCHECK);
-        }
-      }
-      if(sweep)
-      {
-        myservo.attach(PIN);
-      }
-    }
-  }
-  if(sweep)
-  {
-    newpos += sweepval;
-  }
-}
-
 void setup() {                
   STRIP_PINOUT;        // set output pin - DEBUG: should auto detect which mother board for use
   reset_strip(0xFE);
@@ -532,22 +406,15 @@ void setup() {
 
 void loop2() 
 {
-  if(sweep)
-  {
-    runSweep();
-  }
-
   checkForCommand();
 
-  if ((newpos != pos) && myservo.attached()) {
+  if (newpos != pos) {
+    if(!myservo.attached())
+    {
+      myservo.attach(PIN);
+    }
     int newval = a_to_ms(newpos);
     int val = a_to_ms(pos);
-    if(!sweep)
-    {
-      Serial.print(pos);
-      Serial.print(" -> ");
-      Serial.println(newpos);
-    }
     for(; newval < val; val--)
     {
       myservo.writeMicroseconds(val);
@@ -559,11 +426,7 @@ void loop2()
       delay(delayval);
     }
     pos = newpos;
-    if(!sweep)
-    {
-      Serial.println("Detach");
-      myservo.detach();
-    }
+    myservo.detach();
   }
 }
 
